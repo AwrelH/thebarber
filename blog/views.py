c8f6eb9369a8ad7e673e58fa2ffa9a8fa6e472b7
AwrelH <hiwa.awrel@gmail.com>
2022-10-27 11:46:14 +0000

    added blogpostlist, some styling and content to page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import BlogPost
 
-# Create your views here.
+
+class BlogPostList(generic.ListView):
+    model = BlogPost
+    queryset = BlogPost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 9
+    
+
